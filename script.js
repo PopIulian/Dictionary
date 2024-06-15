5577115a107cec145acb44adf5c48f0bccb49106
PopIulian <iulianvaso@yahoo.com>
2024-06-15 21:11:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let dictionary = {};
+
+function addWord() {
+    let word = document.getElementById("add-word").value.trim();
+    if (word === "") {
+        alert("Please enter a word.");
+        return;
+    }
+    dictionary[word.toLowerCase()] = true;
+    document.getElementById("add-word").value = "";
+    document.getElementById("message").textContent ="Word saved successfully!";
+}
+
+function searchWord() {
+    let word = document.getElementById("search-word").value.trim();
+    if (word === "") {
+        alert("Please enter a word.");
+        return;
+    }
+    if (dictionary[word.toLowerCase()]) {
+        document.getElementById("message").textContent ="The word is in the dictionary.";
+    } else {
+        document.getElementById("message").textContent ="The word is not in the dictionary.";
+    }
+}
